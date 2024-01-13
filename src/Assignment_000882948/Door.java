673a5e19f7fb0c751c08e56dd674acb2a2bb5e75
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-01-12 23:20:50 -0500

    Update Door.java

--- src/Assignment_000882948/Door.java
+++ src/Assignment_000882948/Door.java
@@ -1,4 +1,9 @@
-//@author Sujan Rokad, 000882948
+/*
+ * Author:                 Sujan Rokad
+ * Authorship statement:   I, Sujan Rokad, 000882948, certify that this material is my original work.
+ *                         No other person's work has been used without due acknowledgement.
+ * Purpose:                Define a Door class to represent doors within houses.
+ */
 
 package Assignment_000882948;
 
@@ -6,19 +11,34 @@ import javafx.scene.canvas.GraphicsContext;
 
 import static javafx.scene.paint.Color.*;
 
+/**
+ * The Door class represents a door within a house.
+ */
 public class Door {
-    private double x; //house position from top left corner and goes vertically to right side
-    private double y; //house position from top left corner and goes horizontally to bottom
-    private double height; //making variable for height of the door
+    private double x; // X-coordinate of the door
+    private double y; // Y-coordinate of the door
+    private double height; // Height of the door
 
-    public Door(double x, double y, double height){
+    /**
+     * Constructs a door with specified coordinates and height.
+     *
+     * @param x      The X-coordinate of the door.
+     * @param y      The Y-coordinate of the door.
+     * @param height The height of the door.
+     */
+    public Door(double x, double y, double height) {
         this.height = height;
         this.x = x;
         this.y = y;
     }
 
-    public void draw(GraphicsContext gc){
+    /**
+     * Draws the door on the provided GraphicsContext.
+     *
+     * @param gc The GraphicsContext on which to draw the door.
+     */
+    public void draw(GraphicsContext gc) {
         gc.setFill(BLACK);
-        gc.fillRect(x, y, height/1.7, height); //draws the door
+        gc.fillRect(x, y, height / 1.7, height); // Draws the door
     }
 }
